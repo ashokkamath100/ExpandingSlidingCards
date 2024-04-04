37927f23cfe668828f9c933fbaf9b820c60edb46
Ashok Kamath <ashokkamath@Ashoks-MacBook-Pro.local>
2024-04-04 19:26:30 -0400

    finished

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+const container = document.querySelector('.container') ; 
+const panels = document.querySelectorAll('.panel') ; 
+console.log(panels)
+
+panels.forEach((panel)=> {
+    panel.addEventListener('click', () =>
+    {
+        removeActiveclasses()
+        panel.classList.add('active') 
+    })
+}) ; 
+
+function removeActiveclasses()
+{
+    panels.forEach(panel =>
+    {
+        panel.classList.remove('active')
+    })
+}
+
